52e9794926b436eda5b31e360e391e1a89de3696
Mohammed cyclewala <mohammedcyclewala.cm@gmail.com>
2020-06-27 00:36:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import arrayWords from '../utils/words';
+import CountryWords from '../utils/countries';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words='';
+  Countries='';
+  limit=20;
+
+  handleSlideChange(newlimit: number){
+    this.limit=newlimit;
+  }
+  generate(){
+    this.words=arrayWords.slice(0, this.limit).join(' ');
+  }
+  generateRandomCountries(){
+    this.Countries=CountryWords.slice(0, this.limit).join(', ');
+  }
 }
